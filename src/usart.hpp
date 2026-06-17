27d18357c89d966ace34f337fd1c073e92ec683b
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2026-06-17 23:21:13 +0900

    update comment

--- src/usart.hpp
+++ src/usart.hpp
@@ -10,15 +10,19 @@ public:
     UsartManager() : txMutex(nullptr), rxQueue(nullptr) {}
 
     /// @brief 初期化メソッド
+    /// @param baudrate ボーレート
     void init(uint32_t baudrate);
 
-    /// @brief 文字列送信メソッド（どのタスクからでも呼べるグローバル関数用）
+    /// @brief 文字列送信メソッド
+    /// @param str 送信する文字列
     void sendString(const char* str);
 
-    /// @brief 割り込みハンドラから呼ばれる受信用メソッド
+    /// @brief 受信用メソッド
+    /// @note 受信割り込みから呼び出される
     void handleRxFromISR();
 
-    /// @brief 受信タスクのメインループ（内部で呼び出す）
+    /// @brief 受信タスクのメインループ
+    /// @note 内部で呼び出す
     void rxTaskLoop();
 
 private:
