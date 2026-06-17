3534b2e711d321d9b41afebd01918c657e744f90
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2026-06-17 23:22:05 +0900

    add usart init

--- src/usart.cpp
+++ src/usart.cpp
@@ -4,3 +4,49 @@
 #include "queue.h"
 #include "semphr.h"
 #include "usart.hpp"
+
+void UsartManager::init(uint32_t baudrate) {
+    // FreeRTOS リソースの生成
+    txMutex = xSemaphoreCreateMutex();
+    rxQueue = xQueueCreate(RxBufferSize, sizeof(uint8_t));
+
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+    USART_InitTypeDef USART_InitStructure = {0};
+    NVIC_InitTypeDef NVIC_InitStructure = {0};
+
+    // クロック有効化
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
+
+    // TX (PA9) の設定
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // RX (PA10) の設定
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // USART1 設定
+    USART_InitStructure.USART_BaudRate = baudrate;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    USART_Init(USART1, &USART_InitStructure);
+
+    // 受信割り込み有効化
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+    // NVIC 設定
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // USART1 有効化
+    USART_Cmd(USART1, ENABLE);
+}
