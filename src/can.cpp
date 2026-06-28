bcc3225d9c0f475ba305f847824ef3e568be2168
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2026-06-29 00:22:38 +0900

    add canSendMessage()

--- src/can.cpp
+++ src/can.cpp
@@ -1,7 +1,10 @@
+#include <array>
+#include <algorithm>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "ch32v20x_can.h"
 #include "can.h"
+
 
 /// @brief CAN通信の初期化関数
 void canInit() {
@@ -18,6 +21,51 @@ void canInit() {
     }
 
     CAN_Init(CAN1, &CAN_InitStructure);
+}
+
+/// @copydoc canSendMessage(uint32_t, const std::array<uint8_t, 8>&, bool)
+uint8_t canSendMessage(uint32_t id, const std::array<uint8_t, 8> &sendData, bool isExtended) {
+    // データ長が8バイトを超える場合は送信失敗
+    if (sendData.size() > 8) {
+        return -1;
+    }
+
+    // CAN送信メッセージ構造体を設定
+    CanTxMsg CanTxStructure;
+    CanTxStructure.IDE = CAN_Id_Standard;
+    CanTxStructure.RTR = CAN_RTR_Data;
+    CanTxStructure.DLC = sendData.size();
+    // 標準IDまたは拡張IDを設定
+    if (isExtended) {
+        CanTxStructure.IDE = CAN_Id_Extended;
+        CanTxStructure.ExtId = id;
+    } else {
+        CanTxStructure.StdId = id;
+    }
+    // データをコピー
+    auto temp = std::copy(sendData.begin(), sendData.end(), CanTxStructure.Data);
+    if (temp != CanTxStructure.Data + sendData.size()) {
+        return -1; // コピー失敗
+    }
+
+    // CAN送信
+    uint8_t mailbox = CAN_Transmit(CAN1, &CanTxStructure);
+    if (mailbox == CAN_TxStatus_NoMailBox) {
+        return -1; // 送信失敗
+    }
+
+    // 送信カウンタを初期化
+    uint16_t transmitCounter = 0;
+
+    // 送信完了を待つ
+    while (CAN_TransmitStatus(CAN1, mailbox) != CANTXOK){
+        transmitCounter++;
+        if (transmitCounter > CAN_TX_TIMEOUT) {
+            return -1; // 送信失敗
+        }
+    };
+
+    return 0; // 送信成功
 }
 
 /// @copydoc can_task(void*)
--- src/can.h
+++ src/can.h
@@ -1,7 +1,17 @@
 #pragma once
 
+constexpr uint16_t CAN_TX_TIMEOUT = 1000; // CAN送信タイムアウト
+
 /// @brief CAN通信タスクのハンドル
 TaskHandle_t can_task_handle;
+
+/// @brief CANメッセージを送信する関数
+/// @param id 送信するCAN ID
+/// @param sendData 送信するデータ
+/// @param isExtended 拡張IDを使用するかどうか（デフォルトはfalse）
+/// @return 0: 送信成功, -1: 送信失敗
+uint8_t canSendMessage(uint32_t id, const std::array<uint8_t, 8> &sendData, bool isExtended = false);
+
 
 /// @brief CAN通信タスクの関数
 /// @param pvParameters タスクに渡されるパラメータ（今回は使用しない）
